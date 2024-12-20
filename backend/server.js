e5793ea9ab7d64cff1ffdbc9c6677146599cdfb9
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-12-20 16:21:04 +0100

    still on one issue

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ const youtubedl = require("youtube-dl-exec");
 const app = express();
 const PORT = 3000;
 
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.post("/formats", async (req, res) => {
     const { url } = req.body;
-    console.log(url);
+    // console.log(url);
 
     if (!url) {
         return res.status(400).json({ error: "Invalid YouTube URL" });
@@ -42,6 +42,9 @@ app.post("/formats", async (req, res) => {
 // Route to download the video/audio
 app.post("/download", async (req, res) => {
     const { url, format_id } = req.body;
+    console.log(format_id
+
+    )
 
     if (!url || !format_id) {
         return res.status(400).send("Invalid request. Please provide a valid YouTube URL and format.");
@@ -51,10 +54,17 @@ app.post("/download", async (req, res) => {
         const video = youtubedl(url, {
             format: format_id,
             output: '-',
+            noCheckCertificate: true,
+
         });
+        // if (typeof video.pipe !== 'function') {
+        //     throw new Error('Video is not a stream');
+        // }
+
 
         res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
-        video.pipe(res);
+        console.log(video)
+        video.stdout.pipe(res);
     } catch (error) {
         res.status(500).json({ error: "Failed to download video" });
         console.error(error);
